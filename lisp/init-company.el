b3c212c0542e3c20538c6bf9f7ccbf7a1a99abc6
David Ellison <davidehellison@gmail.com>
2022-04-12 08:56:32 -0700

    company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,13 +1,13 @@
 (use-package company
   :ensure t
-  :bind (("C-M-i" . company-complete)
-	 ("M-/" . company-complete))
+  :bind (("M-/" . hippie-expand)
+	 ("C-M-i" . company-complete))
   :init
-  (setq company-idle-delay 0
+  (setq company-idle-delay 0.5
 	company-minimum-prefix-length 1
 	company-require-match nil
 	company-tooltip-align-annotations t
 	company-tooltip-limit 12))
 
-(use-package company-lsp
-  :ensure t)
+;; (use-package company-lsp
+;;   :ensure t)
