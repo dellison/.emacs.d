69d781aa15c3c64d8691b4e673a55da0c2880302
David Ellison <davidehellison@gmail.com>
2022-10-28 16:56:25 -0700

    roam

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -105,6 +105,21 @@ DEADLINE: %^t
   :after org
   :init
   (setq org-roam-directory (file-truename "~/Dropbox/org-roam")
+	org-roam-capture-templates
+	'(("d" "default" plain "%?" :target
+	   (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}
+")
+	   :unnarrowed t)
+	  ("t" "notes on a text" plain "%?\n* Main Argument\n* Supporting Arguments\n* Subjects and Sites\n* Sources\n* Methods\n* Scholarly Debate\n* Terms & Themes\n* Questions\n* Notes\n"
+	   :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}")
+	   :unarrowed t)
+
+	  ("n" "notes on a novel/short story" plain "%?\n* Setting* Plot Summary\n* Characters\n* Themes\n* Questions\n* Notes\n"
+	   :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}")
+	   :unarrowed t)
+	  
+				      )
+
 	org-roam-dailies-directory "daily/"
 	org-roam-dailies-capture-templates
 	'(("d" "default" entry
@@ -112,7 +127,8 @@ DEADLINE: %^t
               :target (file+head "%<%Y-%m-%d>.org"
                                  "#+title: %<%Y-%m-%d>\n"))))
   :config
-  (org-roam-db-autosync-mode))
+  (org-roam-db-autosync-mode)
+  (add-to-list 'org-agenda-files org-roam-directory))
 
 (use-package org-super-agenda
   :ensure t
