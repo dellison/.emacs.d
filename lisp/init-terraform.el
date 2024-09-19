51edd4f4333779ea67d02a88be754c1c836634c5
David Ellison <david@davidellison.org>
2024-09-19 10:49:32 -0700

    use eglot with terraform

--- lisp/init-terraform.el
+++ lisp/init-terraform.el
@@ -1,6 +1,14 @@
+;; terraform!
+
+;; can install tf language server from hashicorp with:
+;; $ brew install hashicorp/tap/terraform-ls
+
 (use-package terraform-mode
   :ensure t
-  :mode (("\\.tf\\'" . terraform-mode)))
+  :mode (("\\.tf\\'" . terraform-mode))
+  :hook ((terraform-mode . eglot-ensure))
+  :config
+  (add-to-list 'eglot-server-programs '(terraform-mode "terraform-ls" "serve")))
 
 (use-package terraform-doc
   :ensure t
