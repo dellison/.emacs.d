ee4532b2c0089024baf76f9e3086d6fded37dcb6
David Ellison <david.ellison@oracle.com>
2024-10-17 11:27:59 -0700

    json-ts alias

--- lisp/init-json.el
+++ lisp/init-json.el
@@ -2,5 +2,6 @@
   :after treesit
   :mode ("\\.json\\'" . json-ts-mode)
   :config
+  (defalias 'json-mode 'json-ts-mode)
   (add-to-list 'treesit-language-source-alist
 	       '(json "https://github.com/tree-sitter/tree-sitter-json")))
